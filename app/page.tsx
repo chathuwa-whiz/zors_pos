7d2df7996e11a4a58ead9acf71e3c0e49d7cfdcf
chathuwa-whiz <navodchathushka@gmail.com>
2025-08-25 11:54:48 +0530

    remove unnecessary .idea configuration files and update layout and page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+export default function HomePage() {
   return (
-      <div>hi cute lamayoo</div>
-  );
+    <div>HomePage</div>
+  )
 }
